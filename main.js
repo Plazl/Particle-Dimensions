4ae20a97702108651069bbb8fa15912482091355
jhnbg rth <I1234itsplaz@outlook.com>
2021-09-08 08:58:26 -0400

    Update main.js

--- main.js
+++ main.js
@@ -12,20 +12,17 @@
     
     var multiplierCost = 100;
 
-  function load() {
-    clicks = JSON.parse(localStorage.getItem('clicks'));
+     clicks = JSON.parse(localStorage.getItem('clicks'));
     multiplier = JSON.parse(localStorage.getItem('multiplier'));
     clickers = JSON.parse(localStorage.getItem('clickers'));
     clickerCost = JSON.parse(localStorage.getItem('clickerCost'));
-    multiplierCost = JSON.parse(localStorage.getItem('multiplierCost'));
-     document.getElementById("multiplierCost").innerHTML = Math.round(multiplierCost);
       document.getElementById("mult").innerHTML = multiplier;
      document.getElementById("clicks").innerHTML = Math.round(clicks);
      document.getElementById("clickerCost").innerHTML = Math.round(clickerCost);
      document.getElementById("clickers").innerHTML = clickers;
      
      
-   }
+   
 
 function addClicks(ammount) {
 
