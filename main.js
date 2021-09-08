9645242eb0523dca015a419d87298d9402134b38
jhnbg rth <I1234itsplaz@outlook.com>
2021-09-08 09:00:11 -0400

    Update main.js

--- main.js
+++ main.js
@@ -12,14 +12,9 @@
     
     var multiplierCost = 100;
 
-     clicks = JSON.parse(localStorage.getItem('clicks'));
-    multiplier = JSON.parse(localStorage.getItem('multiplier'));
-    clickers = JSON.parse(localStorage.getItem('clickers'));
-    clickerCost = JSON.parse(localStorage.getItem('clickerCost'));
+     clicks = JSON.parse(localStorage.getItem('clicks'));;
       document.getElementById("mult").innerHTML = multiplier;
      document.getElementById("clicks").innerHTML = Math.round(clicks);
-     document.getElementById("clickerCost").innerHTML = Math.round(clickerCost);
-     document.getElementById("clickers").innerHTML = clickers;
      
      
    
