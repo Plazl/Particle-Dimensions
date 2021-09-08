8487b6fa0ab072e33bb7d7c67c78f5852c086c72
jhnbg rth <I1234itsplaz@outlook.com>
2021-09-08 08:41:20 -0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,14 @@
+ 
+var hasStarted = false;
+     
+     var interval = 1000;
+
+     var clicks = 0;
+    
+    var clickerCost = 150;
+    
+    var clickers = 0;
+    
+    var multiplier = 1;
+    
+    var multiplierCost = 100;
