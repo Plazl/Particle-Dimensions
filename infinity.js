671ea38a72b8a93fee113da4c671c053f2b48514
jhnbg rth <I1234itsplaz@outlook.com>
2021-11-05 08:20:47 -0400

    Create infinity.js

---
+++ infinity.js
@@ -0,0 +1,22 @@
+
+function infinity() {
+	if(canInfinity = true) {
+	
+		
+		clicks = 10;
+	        multiplier = 1;
+                multiplierCost = 5;
+	        clickers = 0;
+		clickerCost = 5;
+		infiniclicks = infiniclicks + 1 * icmultiplier;
+		t = 1000;
+		canInfinity = false;
+		isFinitee = isFinite(clicks)
+		x.style.display = "none";
+		y.style.display = "block"
+		
+
+
+	   }
+        }
+    
