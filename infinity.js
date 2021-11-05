295197f6de1a171a251d19c24c20485c29a8b01e
jhnbg rth <I1234itsplaz@outlook.com>
2021-11-05 08:22:40 -0400

    Update infinity.js

--- infinity.js
+++ infinity.js
@@ -1,4 +1,30 @@
 
+function finiteCheck() {
+	
+     
+	if (isFinitee == false) {
+		canInfinity = true;
+         x = document.getElementById("infinity");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+	 
+  } 
+		if (y.style.display === "block") {
+    y.style.display = "none";
+	}
+	
+	
+	
+	
+	
+   
+ else  {
+	x.style.display = "none"
+	y.style.display = "block"
+}
+ }
+
+}
 function infinity() {
 	if(canInfinity = true) {
 	
