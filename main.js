b3b04c8189a3a828310fd9319239a987aaf6f8c3
jhnbg rth <I1234itsplaz@outlook.com>
2021-12-10 12:55:31 -0500

    Update main.js

--- main.js
+++ main.js
@@ -376,7 +376,16 @@ function finiteCheck() {
  }
 
 }
-    
+   
+
+
+document.addEventListener('keypress', function (e) {
+            if (e.keyCode === 13 || e.which === 13) {
+                e.preventDefault();
+                return false;
+            }
+            
+        });
 
 window.onLoad = loadSave();
  if (clickers == 1) {
